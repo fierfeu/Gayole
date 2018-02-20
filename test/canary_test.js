b8a4fa5d64384447131fc8e25187949199deb518
fierfeu@gmail.com <Agath0cles>
2018-02-20 23:10:20 +0100

    add canary test for test framwork verification

---
+++ test/canary_test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe("canary test to verify that test framework is available", () => {
+    it("should pass true = true test", () => {
+        expect (true).to.eq(true);
+    })
+})
