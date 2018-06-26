ff7763668c8797058dc7e071db640afe0436c11c
fierfeu <fierfeu@gmail.com>
2018-06-26 18:33:42 +0200

    oubli de virgule ds al conf protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -18,7 +18,7 @@ exports.config = {
     specs: ['test/E2E/*.js'],
 
     capabilities: {
-        'browserName' : 'firefox'
+        'browserName' : 'firefox',
         'moz:firefoxOptions': {
           'args': ['-headless']
         }
