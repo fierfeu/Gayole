932a60371224a164132bdfb05d15dbc1bccd8982
Stéphane Guédon <feirfeu@gamil.com>
2019-02-08 15:03:55 +0100

    Procfile Heroku 4 node

--- src/Servers/App/index.js
+++ src/Servers/App/index.js
@@ -3,7 +3,8 @@
 const web = require('./webHandler.js');
 const http = require('http');
 
-let webserver;
+let webserver; // instance de server web pour les accès en http
+
 
 exports.run = (hostname, port)=> {
     webserver = http.createServer(web.handler).listen(port, hostname);
