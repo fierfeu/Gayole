8d92a1e1ef3b83584f19e2e5c59cb0d214ef8009
fierfeu <fierfeu@gmail.com>
2019-07-01 19:49:18 +0200

    more clear title for test Title

--- test/E2E/homePage-test.js
+++ test/E2E/homePage-test.js
@@ -6,8 +6,8 @@ const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-describe ('http://hostname/ allow acces to home page', ()=> {
-    it('return Gayole Home Page when using root access',(done)=>{
+describe ('http://hostname/ allow acces to home page &', ()=> {
+    it('return Gayole Title',(done)=>{
         browser.get(browser.baseUrl);
         expect(browser.getTitle()).to.eventually.equal('Gayole');
         done();
