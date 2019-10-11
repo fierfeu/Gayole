fe22a6f2dbe2fe7bb1ae5caf2890c707ac986b47
GUEDON Stephane <az02658-dev@corp.noxiane.net>
2019-10-11 09:27:49 +0200

    PageRender Tests

--- test/Servers/App/pageRender_test.js
+++ test/Servers/App/pageRender_test.js
@@ -1,8 +1,43 @@
 'use strict'
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const pageRender = require('../../../src/Servers/App/pageRender.js');
+const fs = require('fs');
 
-// pageRender.render must called res.end()
+var response ={
+    writeHead (statusCode,statusMessage) {
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+    },
+    write (data) {this.data+=data},
+    end () {this.body=this.data},
+};
 
-// pageRender acces to the good file given through target definition param
+
+describe('pageRender.render must return good code', ()=> {
+    it('pageRender must called res.send', ()=>{
+        sinon.spy(response,'end');
+
+        pageRender.render(response);
+
+        expect(response.end.calledOnce).to.be.true;
+
+        response.end.restore();
+    });
+    it('pageRender acces to the good file given through target definition param',()=>{
+        let target='test.test';
+        let targetContent='test Ok';
+        fs.appendFile(target,targetContent, ()=>{});
+
+        pageRender.render(response,target);
+
+        expect(response.body).to.equal(targetContent);
+
+        fs.unlink(target, ()=>{});
+    });
+});
+
+
 
 // pageRender write the good file content
 
