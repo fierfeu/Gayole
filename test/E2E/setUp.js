8c994d220d6da74dd836bd22a971ff6d4c25fd85
GUEDON Stephane <az02658-dev@corp.noxiane.net>
2020-06-04 16:24:38 +0200

    #1 still dragandrop

--- test/E2E/setUp.js
+++ test/E2E/setUp.js
@@ -1,12 +1,24 @@
 'use strict'
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const web = require('../../src/Servers/App/index.js');
+const chrome = require('selenium-webdriver/chrome');
+
+var options = new chrome.Options();
+options.addArguments('start-maximized');
+options.addArguments('disable-popup-blocking');
+options.addArguments('test-type');
+options.addArguments('--no-sandbox');
+options.addArguments("--disable-gpu");
+options.addArguments("--headless"); 
+
 
 const host = (process.env.HOST || 'localhost');
 const port = (process.env.PORT ||80);
 
 before('Before any test suite to create webdriver',async ()=>{
-    global.browser = new Builder().forBrowser('chrome').build();
+    global.browser = new Builder().forBrowser('chrome')
+        .setChromeOptions(options).build();
+     
     console.log('creation with host='+ host +' & PORT = '+ port);
     web.run(host,port);
     browser.baseUrl = "http://"+host+":"+port;
